723dccab58c3bb4ffcc4159837f6e48187e07ba9
Derrick Granowski <granowski@coffeetariat.net>
2023-07-22 14:42:26 -0400

    code cleaning

--- src/main/java/net/coffeetariat/cafe/Application.java
+++ src/main/java/net/coffeetariat/cafe/Application.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
 @SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class Application {
-    public static void main(String[] args) throws JsonProcessingException {
+    public static void main(String[] args) {
         SpringApplication.run(Application.class);
     }
 }
