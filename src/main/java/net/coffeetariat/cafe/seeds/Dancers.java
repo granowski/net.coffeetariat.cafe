9740758b1ae19a27cf43e82d77c1c2111cd3ea98
Derrick Granowski <granowski@coffeetariat.net>
2023-09-24 16:50:15 -0400

    wip; adding sex

--- src/main/java/net/coffeetariat/cafe/seeds/Dancers.java
+++ src/main/java/net/coffeetariat/cafe/seeds/Dancers.java
@@ -2,15 +2,18 @@ package net.coffeetariat.cafe.seeds;
 
 import gov.niem.release.niem.niem_core._5.PersonNameType;
 import gov.niem.release.niem.niem_core._5.PersonType;
+import gov.niem.release.niem.niem_core._5.TextType;
+import jakarta.xml.bind.JAXBElement;
 import net.coffeetariat.cafe.chronometry.NiemDates;
 import net.coffeetariat.cafe.utils.NiemClassTypes;
 
 import javax.xml.datatype.DatatypeConfigurationException;
+import javax.xml.namespace.QName;
 import java.time.Instant;
 import java.util.UUID;
 
 public class Dancers {
-    public static PersonType createAdminDancerNiemPersonType(String fn, String ln, String birthDate, String country) throws DatatypeConfigurationException {
+    public static PersonType createAdminDancerNiemPersonType(String fn, String ln, String birthDate, String country, String sex) throws DatatypeConfigurationException {
 
         var personType = new gov.niem.release.niem.niem_core._5.PersonType();
         personType.setId(UUID.randomUUID().toString());
@@ -24,8 +27,13 @@ public class Dancers {
 
         var pnt = new PersonNameType();
         pnt.getPersonFullName().add(NiemClassTypes.constructPersonNameTextType(fn + " " + ln));
-
         personType.getPersonName().add(pnt);
+
+        var sext = new TextType();
+        sext.setValue(sex);
+        var jsext = new JAXBElement<>(QName.valueOf(""), gov.niem.release.niem.niem_core._5.TextType.class, sext);
+        personType.getPersonSexAbstract().add(jsext);
+
         return personType;
 
     }
